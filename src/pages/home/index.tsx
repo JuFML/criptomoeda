0e10026e61bd2a934fd5b61db5d82f66f62ed935
JuFML <julianafernandezlima@hotmail.com>
2025-03-12 04:23:01 +0100

    feat(home): create input field for the form on the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,31 @@
+import { FormEvent, useState } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 
-export function Home() {
+const Home = () => {
+  const [input, setInput] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (input === "") return
+    navigate(`/detail/${input}`)
+  }
+
+  const handleGetMore = () => {
+    alert("teste")
+  }
 
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Digite o nome da moeda... EX bitcoin"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
@@ -61,6 +77,12 @@ export function Home() {
         </tbody>
       </table>
 
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
+
     </main>
   )
 }
+
+export default Home
